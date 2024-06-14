2dcaa74cc49b2d991dcb513f9eb7a7d39d73287a
princxbadar <daroelz007@gmail.com>
2024-06-14 23:24:29 +0700

    Edit Profile Fixed

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,29 +1,48 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Profile') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-profile-information-form')
+<x-dashboard-layout>
+    <main>
+    <div class="container-fluid px-4">
+        <h1 class="mt-4">Edit Profile</h1>
+        <ol class="breadcrumb mb-4">
+            <li class="breadcrumb-item active">Edit Profile</li>
+        </ol>
+        
+        <!-- Name &Email Edit -->
+        <div class="container">
+            <div class="row">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-body">
+                        @include('profile.partials.update-profile-information-form')            
+                    </div>
                 </div>
             </div>
-
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-password-form')
+            </div>
+        </div> 
+        <!-- Password Edit -->
+        <div class="container mt-3">
+            <div class="row">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-body">
+                        @include('profile.partials.update-password-form')          
+                    </div>
                 </div>
             </div>
-
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.delete-user-form')
-                </div>
             </div>
         </div>
+         <!--Delete Account  -->
+        <div class="container mt-3 mb-3">
+            <div class="row">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-body">
+                        @include('profile.partials.delete-user-form')          
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div> 
+
     </div>
-</x-app-layout>
+    </main>
+</x-dashboard-layout>
