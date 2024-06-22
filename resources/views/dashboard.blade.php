342ba5932b1cd7a5a989269c1487fe865d5308d6
princxbadar <daroelz007@gmail.com>
2024-06-22 18:47:02 +0700

    added dashboard leaflet maps

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,19 @@
         
         
         <div>
-            <x-leaflet-maps></x-leaflet-maps>
+            <!-- Peta Leaflet -->
+            <div id="map" style="width: 800px; height: 400px;"></div>
+                <script> 
+
+                var map = L.map('map').setView([-6.706117145755258, 108.55783199712985], 13); 
+
+                var tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { 
+                maxZoom: 19, 
+                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>' 
+                }).addTo(map); 
+
+
+                </script>
         </div>
     </div>
 </main>
