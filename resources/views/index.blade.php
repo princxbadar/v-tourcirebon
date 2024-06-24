94faec3cc78f994a98427b34558c038c98c6369f
aryankawibowo <aryankawibowo@gmail.com>
2024-06-24 22:08:16 +0700

    Index page improvement

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,28 +8,31 @@
         <meta name="author" content="" />
         <title>{{ config('app.name', 'Laravel') }}</title>
         <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="crossorigin=""/>
-        <link href="{{asset('assets/style.css')}}" rel="stylesheet" />
+        <link href="{{asset('css/style.css')}}" rel="stylesheet" />
         <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
         @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 
     </head>
     <body>
         <main>
-                <div class="banner">
-                    <img src="{{ asset('image/background.jpg') }}"  alt="" style="max-width: 100%; max-height:100%;" >
-                    <div class="banner-text text-center">
-
+            <div
+                class="bg-image d-flex justify-content-center align-items-center"
+                style="
+                    background-image: url('{{ asset("image/background.jpg") }}');
+                    height: 100vh;
+                "
+            >
+                <div class="text-center text-gray-900 bg-light rounded p-5 opacity-100">
                     <h1>V-TOUR</h1>
                     <h4>Virtual Tour Kota Cirebon</h4>
-                    <a href="{{route('homepage')}}" class="btn btn-primary">Jelajahi</a>
-                    </div>
+                    <p>
+                        Jelajahi pariwisata Kota Cirebon, kunjungi objek - objek wisata yang banyak nan indah dari Kota Cirebon.
+                        <br>
+                        Alami Kota Cirebon dengan cara baru melalui virtual tour.
+                    </p>
+                    <a href="{{route('homepage')}}" class="btn btn-outline-primary">Jelajahi</a>
                 </div>
-
-
-
-
-
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+            </div>
             <script src="js/scripts.js"></script>
         </main>
     </body>
